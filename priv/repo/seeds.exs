4ae7cbdcd6bf5f59e2790cb4f96f5a96ae302673
liamhuynhf <tyhuynh191199@gmail.com>
2025-05-04 21:33:28 +0700

    add sample user accounts with seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,43 @@
 # Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     DigitalAssets.Repo.insert!(%DigitalAssets.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+#   mix run priv/repo/seeds.exs
+
+alias DigitalAssets.Accounts.User
+alias DigitalAssets.Repo
+
+# Define default users
+default_users = [
+  %{
+    email: "admin@gmail.com",
+    name: "Admin",
+    role: "admin",
+    password: "admin"
+  },
+  %{
+    email: "creator@gmail.com",
+    name: "Creator",
+    role: "creator",
+    password: "creator"
+  },
+  %{
+    email: "customer@gmail.com",
+    name: "Customer",
+    role: "customer",
+    password: "customer"
+  }
+]
+
+# Create users if they don't exist
+Enum.each(default_users, fn user_params ->
+  # Check if user already exists
+  case Repo.get_by(User, email: user_params.email) do
+    nil ->
+      %User{}
+      |> User.changeset(user_params)
+      |> Repo.insert!()
+
+      Mix.shell().info("Created #{user_params.role} user: #{user_params.email}")
+
+    _user ->
+      Mix.shell().info("User #{user_params.email} already exists, skipping")
+  end
+end)
